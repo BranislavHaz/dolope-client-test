247a09ee76cb52d3d601a7cd8fc88b9c86f40599
Branislav Ház <haz.branislav@gmail.com>
2024-08-17 21:28:43 +0200

    Add new url to gtm script

--- pages/_document.js
+++ pages/_document.js
@@ -35,11 +35,7 @@ export default class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-P7QZ2VKM');
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://gtm.dolope.cz/lqzvcagp.js?st='+i+dl+'';f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','P7QZ2VKM');
             `,
             }}
           />
@@ -53,10 +49,10 @@ export default class MyDocument extends Document {
         <body>
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-P7QZ2VKM"
+              src="https://gtm.dolope.cz/ns.html?id=GTM-P7QZ2VKM"
               height="0"
               width="0"
-              style={{ display: "none", visibility: "hidden" }}
+              style="display:none;visibility:hidden"
             ></iframe>
           </noscript>
           <Main />
