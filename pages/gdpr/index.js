9befb73c7270bb59784faec1e7efa40115546ed5
Branislav Ház <haz.branislav@gmail.com>
2024-08-20 17:29:30 +0200

    Edit gdpr and htaccess

--- pages/gdpr/index.js
+++ pages/gdpr/index.js
@@ -1,7 +1,7 @@
 import Header from "@/layouts/gdpr/Header";
 import Main from "@/layouts/gdpr/Main";
-import GDPRsk from "./GDPRsk";
-import GDPRcz from "./GDPRcz";
+import GDPRsk from "../../components/gdpr/GDPRsk";
+import GDPRcz from "../../components/gdpr/GDPRcz";
 const language = process.env.NEXT_PUBLIC_LANGUAGE;
 
 const Content = language === "cz" ? GDPRcz : GDPRsk;
