19325b7ca578f23b95b9b966f4cc59ffccb325bc
Branislav Ház <haz.branislav@gmail.com>
2024-09-01 21:30:37 +0200

    Edit structure

--- pages/gdpr/index.js
+++ pages/gdpr/index.js
@@ -1,28 +1,40 @@
 import Head from "next/head";
-import Header from "@/layouts/gdpr/Header";
-import Main from "@/layouts/gdpr/Main";
-import GDPRsk from "../../components/gdpr/GDPRsk";
-import GDPRcz from "../../components/gdpr/GDPRcz";
-import skLocale from "@/locales/gdpr/sk.json";
-import czLocale from "@/locales/gdpr/cz.json";
-const language = process.env.NEXT_PUBLIC_LANGUAGE;
+import Content from "@/layouts/web/Content";
+import GDPRsk from "@/components/web/gdpr/GDPRsk";
+import GDPRcz from "@/components/web/gdpr/GDPRsk";
 
-const Content = language === "cz" ? GDPRcz : GDPRsk;
+import skLocale from "@/locales/web/gdpr/sk.json";
+import czLocale from "@/locales/web/gdpr/cz.json";
+import skLayout from "@/locales/layouts/web/sk.json";
+import czLayout from "@/locales/layouts/web/cz.json";
+
+const language = process.env.NEXT_PUBLIC_LANGUAGE;
+const ContentText = language === "cz" ? GDPRcz : GDPRsk;
 const translate = language === "cz" ? czLocale : skLocale;
 
-const GDPR = () => {
+const GDPR = ({ translationsLayout }) => {
   return (
     <>
       <Head>
         <title>{`Dolope | ${translate.title}`}</title>
         <meta name="description" content={translate.meta_description} />
       </Head>
-      <Header />
-      <Main>
-        <Content />
-      </Main>
+      <Content translations={translationsLayout}>
+        <ContentText />
+      </Content>
     </>
   );
 };
+
+export async function getStaticProps() {
+  const language = process.env.NEXT_PUBLIC_LANGUAGE;
+  const translationsLayout = language === "cz" ? czLayout : skLayout;
+
+  return {
+    props: {
+      translationsLayout,
+    },
+  };
+}
 
 export default GDPR;
